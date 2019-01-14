b51a37bbab3c488eb90128f6614ce7ba8fbad2fe
Peter Coleman <peter.coleman143@gmail.com>
2019-01-14 12:58:19 +1000

    Sharing verses between theads and passing query to search function

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@
 extern crate iron;
 extern crate router;
 extern crate url;
+extern crate persistent;
+
+#[macro_use]
+extern crate lazy_static;
 
 mod bible;
 mod search;
@@ -14,49 +18,51 @@ use std::vec::Vec;
 use std::process;
 use std::time::Instant;
 use bible::Bible;
+use bible::Verse;
 use search::search;
 use std::io;
 use iron::prelude::*;
 use iron::status;
+use iron::{Iron, Chain};
 use router::Router;
 use url::Url;
+use persistent::Read;
+use std::sync::Arc;
+use iron::{typemap, AfterMiddleware, BeforeMiddleware};
+
+#[derive(Debug)]
+struct SharedVerses<'a> {
+    verses: Vec<Verse<'a>>
+}
+
+/// Allows storing `SharedVerses` in Iron's persistent data structure.
+/// This needs to be static
+impl ::iron::typemap::Key for SharedVerses<'static> {
+    type Value = SharedVerses<'static>;
+}
 
 fn main() -> io::Result<()> {
+    // lazy Load bible as static
+    lazy_static! {
+        static ref BIBLE: Bible = match Bible::new("src/data/t_asv.csv") {
+            Ok(b) => b,
+            Err(_) => process::exit(1)
+        };
+    }
+
+    // Get verses from bible
+    let shared_verses = SharedVerses { verses: BIBLE.get_verses() };
 
     let mut router = Router::new();  
     router.get("/:bible", handler, "bible");
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    let mut chain = Chain::new(router);
+    chain.link(Read::<SharedVerses>::both(shared_verses));
 
-    let bible = match Bible::new("src/data/t_asv.csv") {
-        Ok(bible) => bible,
-        Err(_) => process::exit(1)
-    };
+    // Prepare server
+    Iron::new(chain).http("localhost:3000").unwrap();
 
-    let verses = bible.get_verses();
-
-    loop {
-        // 1. Ask for each string
-        println!("\nSearch verses: ");
-
-        // 2. Read input
-        let mut query = String::new();
-        io::stdin().read_line(&mut query)
-            .expect("Failed to read line");
-
-        // 5. If no input was given exit the program
-        if query.chars().count() == 0  {
-            process::exit(1);
-        }
-
-        let now = Instant::now();
-        let matches = search(query, &verses);
-        println!("{:?}", now.elapsed());
-
-        for m in matches {
-            println!("{:?}\n", m);
-        }
-    }
+    Ok(())
 }
 
 fn handler(req: &mut Request) -> IronResult<Response> {
@@ -70,6 +76,14 @@ fn handler(req: &mut Request) -> IronResult<Response> {
         Some(st) => st,
         None => process::exit(1)
     };
+
+    let shared_verses = req.get::<Read<SharedVerses>>().unwrap();
+
+    let matches = search(value.to_string(), &shared_verses.verses);
+
+    for m in matches {
+        println!("{:?}", m);
+    }
 
     Ok(Response::with((status::Ok, value.to_string())))
 }
