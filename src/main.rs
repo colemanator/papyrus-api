126d8bc187d50c3d48d44b9d943b4cad3b293fa0
Peter Coleman <peter.coleman143@gmail.com>
2019-02-15 13:53:23 +1000

    added support for env vars and added errors responses

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate url;
 extern crate persistent;
 extern crate serde;
 extern crate serde_json;
+extern crate dotenv;
 
 #[macro_use]
 extern crate lazy_static;
@@ -20,11 +21,13 @@ mod search;
 mod normalise;
 
 use std::vec::Vec;
+use std::env;
 use std::process;
 use std::time::Instant;
 use bible::Bible;
 use bible::Verse;
 use search::search;
+use search::Match;
 use std::io;
 use iron::prelude::*;
 use iron::status;
@@ -36,6 +39,8 @@ use std::sync::Arc;
 use iron::{typemap, AfterMiddleware, BeforeMiddleware};
 use iron::headers::{AccessControlAllowOrigin, ContentType};
 use iron::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+use dotenv::dotenv;
+use serde_json::Result;
 
 #[derive(Debug)]
 struct SharedVerses<'a> {
@@ -51,6 +56,22 @@ struct MatchedVerse {
     verse: u8
 }
 
+// After middleware for headers
+struct Headers;
+impl AfterMiddleware for Headers {
+    /// Here we set the standard headers for our API
+    fn after(&self, req: &mut Request, mut res: Response) -> IronResult<Response> {
+        res.headers.set(
+            AccessControlAllowOrigin::Value(env::var("APP_URL").unwrap().to_owned())
+        );
+        res.headers.set(
+            ContentType(Mime(TopLevel::Application, SubLevel::Json,vec![(Attr::Charset, Value::Utf8)]))
+        );
+
+        Ok(res)
+    }
+}
+
 /// Allows storing `SharedVerses` in Iron's persistent data structure.
 /// This needs to be static
 impl ::iron::typemap::Key for SharedVerses<'static> {
@@ -58,6 +79,9 @@ impl ::iron::typemap::Key for SharedVerses<'static> {
 }
 
 fn main() -> io::Result<()> {
+    // Load environment variables from .env
+    dotenv().ok();
+
     // lazy Load bible as static
     lazy_static! {
         static ref BIBLE: Bible = match Bible::new("src/data/t_asv.csv") {
@@ -76,9 +100,10 @@ fn main() -> io::Result<()> {
     // Add router to middleware chain as well as shared verses
     let mut chain = Chain::new(router);
     chain.link(Read::<SharedVerses>::both(shared_verses));
+    chain.link_after(Headers);
 
     // Start server
-    Iron::new(chain).http("localhost:9000").unwrap();
+    Iron::new(chain).http(env::var("API_URL").unwrap()).unwrap();
 
     Ok(())
 }
@@ -86,10 +111,48 @@ fn main() -> io::Result<()> {
 /// Handles incoming requests to a specific route
 fn handler(req: &mut Request) -> IronResult<Response> {
     // Get the query and search verses
-    let query = get_query(req);
+    let query = match get_query(req) {
+        Some(query) => query,
+        None => {
+            return Ok(Response::with((status::BadRequest, "You must provide a query")));
+        }
+    };
+
+    // Perform the search on shared verses
     let shared_verses = req.get::<Read<SharedVerses>>().unwrap();
     let matches = search(query, &shared_verses.verses);
 
+    // Convert the results to JSON
+    let match_json = match match_to_json(matches) {
+        Ok(json) => json,
+        Err(_) => {
+            return Ok(Response::with((status::InternalServerError, "Unable to serialize to JSON;")));
+        }
+    };
+
+    // Create response
+    Ok(Response::with((status::Ok, match_json)))
+}
+
+/// Find and return the query param value with the name `query`
+fn get_query(req: &mut Request) -> Option<String> {
+    let url: Url = req.url.clone().into();
+        
+    let query = url
+        .query_pairs()
+        .find(|(name, value)| name == "query");
+
+    let (name, value) = match query {
+        Some(st) => st,
+        None => {
+            return None;
+        }
+    };
+
+    Some(value.to_string())
+}
+
+fn match_to_json(matches: Vec<Match>) -> Result<String> {
     // Convert to response structure
     let search_matches: Vec<MatchedVerse> = matches
         .iter()
@@ -102,36 +165,5 @@ fn handler(req: &mut Request) -> IronResult<Response> {
         })
         .collect();
 
-    let match_json = match serde_json::to_string(&search_matches) {
-        Ok(json) => json,
-        Err(_) => process::exit(1)
-    };
-
-    // Create response and set headers
-    let mut res = Response::with((status::Ok, match_json));
-    
-    res.headers.set(
-        AccessControlAllowOrigin::Value("http://localhost:3000".to_owned())
-    );
-    res.headers.set(
-        ContentType(Mime(TopLevel::Application, SubLevel::Json,vec![(Attr::Charset, Value::Utf8)]))
-    );
-
-    Ok(res)
-}
-
-/// Find and return the query param value with the name `query`
-fn get_query(req: &mut Request) -> String {
-    let url: Url = req.url.clone().into();
-        
-    let query = url
-        .query_pairs()
-        .find(|(name, value)| name == "query");
-
-    let (name, value) = match query {
-        Some(st) => st,
-        None => process::exit(1)
-    };
-
-    value.to_string()
+    serde_json::to_string(&search_matches)
 }
